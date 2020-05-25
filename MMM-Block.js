e32b2c790adeda7a2938723dd576daa9927cbb73
B. Keyport <brendan.keyport@gmail.com>
2020-05-24 21:26:11 -0700

    update for new OMX size

--- MMM-Block.js
+++ MMM-Block.js
@@ -9,7 +9,8 @@ Module.register("MMM-Block", {
   // Override dom generator.
   getDom: function() {
 	var wrapper = document.createElement("div");
-    wrapper.innerHTML = "<table border=1 style='table-layout:fixed width=301px'><tr style='height:320px'><td style='text-align:center'>Reserved for Camera</td></tr></table>";
+    wrapper.innerHTML = "<table border=1 style='table-layout:fixed width=301px'><tr style='height:394px'><td style='text-align:center'>Reserved for Camera</td></tr></table>";
     return wrapper;
   }
-});
+}
+);
