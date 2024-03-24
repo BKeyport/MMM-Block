d5068d50ac0cbace70448a15c9120852d0475927
dathbe <github@beffa.us>
2024-03-23 22:22:58 -0700

    Update MMM-Block.js
    remove bg color

--- MMM-Block.js
+++ MMM-Block.js
@@ -13,7 +13,7 @@ Module.register("MMM-Block", {
   getDom: function() {
 	var wrapper = document.createElement("div");
     //wrapper.innerHTML = "<table border=0 style='table-layout:fixed width=301px'><tr style='height:" + this.config.height + "px'><td style='text-align:center'>&nbsp;</td></tr></table>";
-    wrapper.innerHTML = "<div style='height:" + this.config.height + "px; padding: 0; background-color: #333'></div>";
+    wrapper.innerHTML = "<div style='height:" + this.config.height + "px; padding: 0'></div>";
     return wrapper;
   }
 }
